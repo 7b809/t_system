78b4e841ac1d8559e7749b9ca371e3917cc0635d
7b809 <ej818793@gmail.com>
2026-04-21 22:14:49 +0530

    Auto update

--- T_System/urls.py
+++ T_System/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("T_System is running 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webhook/', include('webhook_app.urls')),
-    path("", include("ws_feed.urls")),
-
+    path('ws/', include('ws_feed.urls')),  # better to namespace
+    path('', home),  # ✅ root health check
 ]
